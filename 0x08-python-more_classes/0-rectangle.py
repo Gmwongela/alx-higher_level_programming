600eaed46bb7b783496e4cbcc5f1c653f5dcaafc
Gmwongela <gvidla73@gmail.com>
2023-07-04 06:45:28 +0000

    Python - More Classes and Objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""An empty class that defines a Rectangle"""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
