3c9f7967403f8118540e6fadc695eda8367f0f12
Gmwongela <gvidla73@gmail.com>
2023-07-10 14:00:12 +0000

    Python - Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that defines an object with the lookup method"""
+
+
+def lookup(obj):
+    """Function that return the attributes for an object"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that defines a base geometry class BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    @classmethod
+    def area(self):
+        """method for calculated area"""
+
+        raise Exception("area() is not implemented")
